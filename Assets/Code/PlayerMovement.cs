95f5d8173adcac2326c38471736b03db46d6c90a
Wyatt Cook <wcookeverton@gmail.com>
2017-02-06 11:23:51 -0600

    Modularized Update() into 3 functions

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -12,77 +12,84 @@ public class PlayerMovement : MonoBehaviour {
 	void Start () {
         moving = false;
 	}
-	
+	void handleKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            moving = true;
+            direction = 2;
+            Debug.Log("OG x goal:");
+            Debug.Log(goalX);
+            goalX = transform.position.x + .25f * blockSize;
+            Debug.Log("NEW x goal");
+            Debug.Log(goalX);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            moving = true;
+            direction = 0;
+            goalX = transform.position.x - .25f * blockSize;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            moving = true;
+            direction = 1;
+            goalY = transform.position.y + .25f * blockSize;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            moving = true;
+            direction = 3;
+            goalY = transform.position.y - .25f * blockSize;
+        }
+    }
+    void movePlayer()
+    {
+        float desiredX = transform.position.x;
+        float desiredY = transform.position.y;
+        if (direction == 0)
+        {
+            desiredX = transform.position.x - movementSpeed * Time.deltaTime;
+            if (desiredX <= goalX)
+            {
+                moving = false;
+            }
+        }
+        else if (direction == 2)
+        {
+            desiredX = transform.position.x + movementSpeed * Time.deltaTime;
+            if (desiredX >= goalX)
+            {
+                moving = false;
+            }
+        }
+        else if (direction == 1)
+        {
+            desiredY = transform.position.y + movementSpeed * Time.deltaTime;
+            if (desiredY >= goalY)
+            {
+                moving = false;
+            }
+        }
+        else
+        {
+            desiredY = transform.position.y - movementSpeed * Time.deltaTime;
+            if (desiredY <= goalY)
+            {
+                moving = false;
+            }
+        }
+        transform.position = new Vector3(desiredX, desiredY);
+    }
 	// Update is called once per frame
 	internal void Update () {
         if (moving)
         {
-            float desiredX = transform.position.x;
-            float desiredY = transform.position.y;
-            if (direction == 0)
-            {
-                desiredX = transform.position.x - movementSpeed * Time.deltaTime;
-                if (desiredX <= goalX)
-                {
-                    moving = false;
-                }
-            }
-            else if (direction == 2)
-            {
-                desiredX = transform.position.x + movementSpeed * Time.deltaTime;
-                if (desiredX >= goalX)
-                {
-                    moving = false;
-                }
-            }
-            else if (direction == 1)
-            {
-                desiredY = transform.position.y + movementSpeed * Time.deltaTime;
-                if (desiredY >= goalY)
-                {
-                    moving = false;
-                }
-            }
-            else
-            {
-                desiredY = transform.position.y - movementSpeed * Time.deltaTime;
-                if (desiredY <= goalY)
-                {
-                    moving = false;
-                }
-            }
-            transform.position = new Vector3(desiredX, desiredY);
+            movePlayer();
         }
         else
         {
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                moving = true;
-                direction = 2;
-                Debug.Log("OG x goal:");
-                Debug.Log(goalX);
-                goalX = transform.position.x + .25f * blockSize;
-                Debug.Log("NEW x goal");
-                Debug.Log(goalX);
-            }
-            else if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                moving = true;
-                direction = 0;
-                goalX = transform.position.x - .25f * blockSize;
-            }
-            else if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                moving = true;
-                direction = 1;
-                goalY = transform.position.y + .25f * blockSize;
-            }
-            else if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                moving = true;
-                direction = 3;
-                goalY = transform.position.y - .25f * blockSize;
-            }
+            handleKeyInput();
         }
     }
 }
